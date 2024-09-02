bdcec452c354fea468125cfe4c73eef5aa9eae77
OrangeDiaries <44355631+oaleev@users.noreply.github.com>
2024-09-02 18:29:05 -0400

    Final test for the evening

--- src/main/java/com/devsecops/NumericController.java
+++ src/main/java/com/devsecops/NumericController.java
@@ -22,7 +22,7 @@ public class NumericController {
 
 		@GetMapping("/")
 		public String welcome() {
-			return "The DevSecOps";
+			return "DevSecOps is cool...";
 		}
 
 		@GetMapping("/compare/{value}")
