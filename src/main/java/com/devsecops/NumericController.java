797a781ef717abf8b06bcee6e3e63bc9a4df2745
OrangeDiaries <44355631+oaleev@users.noreply.github.com>
2024-09-02 16:52:48 -0400

    New Message

--- src/main/java/com/devsecops/NumericController.java
+++ src/main/java/com/devsecops/NumericController.java
@@ -29,9 +29,9 @@ public class NumericController {
 		public String compareToFifty(@PathVariable int value) {
 			String message = "Could not determine comparison";
 			if (value > 50) {
-				message = "Greater than 50";
+				message = "Yes, Greater than 50";
 			} else {
-				message = "Smaller than or equal to 50";
+				message = "Yes, Smaller than or equal to 50";
 			}
 			return message;
 		}
