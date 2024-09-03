345a5d5f5cc949086688d788a7b4e500e3f939bc
OrangeDiaries <44355631+oaleev@users.noreply.github.com>
2024-09-03 12:12:29 -0400

    Updating the test

--- src/test/java/com/devsecops/NumericApplicationTests.java
+++ src/test/java/com/devsecops/NumericApplicationTests.java
@@ -1,7 +1,7 @@
 package com.devsecops;
 
 
-import org.junit.Test;
+// import org.junit.Test;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
