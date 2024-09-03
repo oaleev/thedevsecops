0fbcc92e3e10f7426fd65bbe61d3da21aa44c00d
OrangeDiaries <44355631+oaleev@users.noreply.github.com>
2024-09-03 13:43:27 -0400

    Test

--- src/test/java/com/devsecops/NumericApplicationTests.java
+++ src/test/java/com/devsecops/NumericApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-import de.flapdoodle.embed.vlmq.MockWebServer;
+// import de.flapdoodle.embed.vlmq.MockWebServer;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
