5bbaffabe2149dcd8d5d5838036b49115c000d63
OrangeDiaries <44355631+oaleev@users.noreply.github.com>
2024-09-03 14:52:57 -0400

    Back to normal

--- src/test/java/com/devsecops/NumericApplicationTests.java
+++ src/test/java/com/devsecops/NumericApplicationTests.java
@@ -1,45 +1,43 @@
 package com.devsecops;
 
 
-// import org.junit.Test;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-// import de.flapdoodle.embed.vlmq.MockWebServer;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+//import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+//import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.reactive.server.WebTestClient;
-import org.springframework.web.util.UriComponentsBuilder;
-import org.springframework.boot.test.web.client.WebTestClient;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = NumericApplicationTests.class)
-// @AutoConfigureMockMvc
+@SpringBootTest
+@AutoConfigureMockMvc
 public class NumericApplicationTests {
 
     @Autowired
-    private WebTestClient webClient;
+    private MockMvc mockMvc;
 
     @Test
-    // public void smallerThanOrEqualToFiftyMessage() throws Exception {
-    public void smallerThanOrEqualToFiftyMessage() {
-         WebTestClient.RequestBuilders.get()
-            .uri("/compare/50")
-            .exchange()
-            .expectStatus().isOk()
-            .expectBody(String.class)
-            .isEqualTo("Yes, Smaller than or equal to 50");
+    public void smallerThanOrEqualToFiftyMessage() throws Exception {
+        this.mockMvc.perform(get("/compare/50")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string("Yes, Smaller than or equal to 50"));
     }
 
     @Test
@@ -50,10 +48,10 @@ public class NumericApplicationTests {
     
     
 
-    @Test
+@Test
     public void welcomeMessage() throws Exception {
         this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
-                .andExpect(content().string("DevSecOps is so cool"));
+                .andExpect(content().string("DevSecOps is cool"));
     }
     
 
