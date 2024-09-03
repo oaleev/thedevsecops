95810d59052534bcc5d2e2cdc682bd1e10203182
OrangeDiaries <44355631+oaleev@users.noreply.github.com>
2024-09-03 13:31:43 -0400

    Updated class in the test

--- src/test/java/com/devsecops/NumericApplicationTests.java
+++ src/test/java/com/devsecops/NumericApplicationTests.java
@@ -18,7 +18,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = NumericApplicationTests.class)
 @AutoConfigureMockMvc
 public class NumericApplicationTests {
 
