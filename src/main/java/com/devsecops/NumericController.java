899af13bdc5a3067f83fc0417ff726c502aa6ff6
OrangeDiaries <44355631+oaleev@users.noreply.github.com>
2024-09-11 18:19:03 -0400

    Copy from Main

--- src/main/java/com/devsecops/NumericController.java
+++ src/main/java/com/devsecops/NumericController.java
@@ -12,7 +12,8 @@ import org.springframework.web.client.RestTemplate;
 public class NumericController {
 
 	private final Logger logger = LoggerFactory.getLogger(getClass());
-	private static final String BASEURL = "http://node-service:5000/plusone";
+	private static final String baseURL = "http://node-service:5000/plusone";
+	//private static final String baseURL = "http://localhost:5000/plusone";
 	
 	RestTemplate restTemplate = new RestTemplate();
 	
@@ -21,23 +22,23 @@ public class NumericController {
 
 		@GetMapping("/")
 		public String welcome() {
-			return "Kubernetes DevSecOps";
+			return "DevSecOps is cool...";
 		}
 
 		@GetMapping("/compare/{value}")
 		public String compareToFifty(@PathVariable int value) {
 			String message = "Could not determine comparison";
 			if (value > 50) {
-				message = "Greater than 50";
+				message = "Yes, Greater than 50";
 			} else {
-				message = "Smaller than or equal to 50";
+				message = "Yes, Smaller than or equal to 50";
 			}
 			return message;
 		}
 
 		@GetMapping("/increment/{value}")
 		public int increment(@PathVariable int value) {
-			ResponseEntity<String> responseEntity = restTemplate.getForEntity(BASEURL + '/' + value, String.class);
+			ResponseEntity<String> responseEntity = restTemplate.getForEntity(baseURL + '/' + value, String.class);
 			String response = responseEntity.getBody();
 			logger.info("Value Received in Request - " + value);
 			logger.info("Node Service Response - " + response);
